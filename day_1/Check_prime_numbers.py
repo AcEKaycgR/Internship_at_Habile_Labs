db60b86cd61ac7c74d9cd29f101552ffa96aced9
Kintan_Ace <61554695+AcEKaycgR@users.noreply.github.com>
2025-06-23 12:31:41 +0530

    Update Check_prime_numbers.py

--- day_1/Check_prime_numbers.py
+++ day_1/Check_prime_numbers.py
@@ -1,13 +1,16 @@
-def prime(n):
-    if n < 2:
-        return "Not Prime"
-    else:
-        for i in range(2, int(n ** 0.5) + 1):
-            if n % i == 0:
-                return "Not prime"
-            else:
-                return "Prime"
+try:
+    def prime(n):
+        if n < 2:
+            return "Not Prime"
+        else:
+            for i in range(2, int(n ** 0.5) + 1):
+                if n % i == 0:
+                    return "Not prime"
+                else:
+                    return "Prime"
 
 
-Num = int(input("Enter a number: "))
-print(prime(Num))
+    Num = int(input("Enter a number: "))
+    print(prime(Num))
+except ValueError:
+    print("Please enter a valid integer.")
