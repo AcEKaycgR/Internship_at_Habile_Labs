4aedbaa27b75a5a30823e610a278d2a296ae7ea0
Kintan_Ace <61554695+AcEKaycgR@users.noreply.github.com>
2025-06-23 12:37:09 +0530

    Update Sum_of_even_numbers.py

--- day_1/Sum_of_even_numbers.py
+++ day_1/Sum_of_even_numbers.py
@@ -1,3 +1,5 @@
-Nums = list(map(int, input("Enter all numbers: ").split()))
-even_sum = sum(x for x in Nums if x % 2 == 0)
-print(f"Even sum: {even_sum}")
+try:
+    nums = list(map(int, input("Enter numbers separated by space: ").split()))
+    print("Sum of even numbers:", sum(x for x in nums if x % 2 == 0))
+except ValueError:
+    print("Please enter valid integers.")
