e2ab5e790c6ca103c6e10b45f24918f716b3587d
Kintan_Ace <61554695+AcEKaycgR@users.noreply.github.com>
2025-06-23 12:34:58 +0530

    Update List_all_divisors.py

--- day_1/List_all_divisors.py
+++ day_1/List_all_divisors.py
@@ -1,5 +1,8 @@
-Num = int(input("Enter a number: "))
+try:
+    Num = int(input("Enter a number: "))
 
-for i in range(1,Num + 1):
-    if Num % i == 0:
-        print(i)
+    for i in range(1, Num + 1):
+        if Num % i == 0:
+            print(i)
+except ValueError:
+    print("Please enter a valid number.")
