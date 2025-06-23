d592508b427685e5d281eb05aea561f4db5d79dc
Kintan_Ace <61554695+AcEKaycgR@users.noreply.github.com>
2025-06-23 12:34:48 +0530

    Update Guess_the_numbers.py

--- day_1/Guess_the_numbers.py
+++ day_1/Guess_the_numbers.py
@@ -1,10 +1,13 @@
 import random
 
-secret = random.randint(1, 100)
-while True:
-    guess = int(input("Your guess"))
-    if secret == guess:
-        print("correct")
-        break
-    else:
-        print("Try again")
+try:
+    secret = random.randint(1, 100)
+    while True:
+        guess = int(input("Your guess"))
+        if secret == guess:
+            print("correct")
+            break
+        else:
+            print("Try again")
+except ValueError:
+    print("Enter a valid number between 1–100.")
