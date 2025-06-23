c5001cfa9132bebe0902b03451189471172f0f36
Kintan_Ace <61554695+AcEKaycgR@users.noreply.github.com>
2025-06-23 12:35:37 +0530

    Update Palindrome_checker.py

--- day_1/Palindrome_checker.py
+++ day_1/Palindrome_checker.py
@@ -1,2 +1,5 @@
-word = input("Enter a word: ")
-print("Palindrome" if word == word[::-1] else "Not palindrome")
+try:
+    word = input("Enter a word: ").lower()
+    print("Palindrome" if word == word[::-1] else "Not a palindrome")
+except Exception as e:
+    print("Error:", e)
