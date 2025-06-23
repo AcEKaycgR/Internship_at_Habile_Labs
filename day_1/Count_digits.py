82392845fbd3765c4943c6a6d9969af3ab03ecea
Kintan_Ace <61554695+AcEKaycgR@users.noreply.github.com>
2025-06-23 12:32:13 +0530

    Update Count_digits.py

--- day_1/Count_digits.py
+++ day_1/Count_digits.py
@@ -1,15 +1,18 @@
-Num = int(input("Enter a number: "))
+try:
+    Num = int(input("Enter a number: "))
 
 
-def count(n):
-    n = abs(n)
-    digit_count = 0
-    if n == 0:
-        return 1
-    while n > 0:
-        n = n // 10
-        digit_count += 1
-    return digit_count
+    def count(n):
+        n = abs(n)
+        digit_count = 0
+        if n == 0:
+            return 1
+        while n > 0:
+            n = n // 10
+            digit_count += 1
+        return digit_count
 
 
-print(count(Num))
+    print(count(Num))
+except Exception as e:
+    print("Error:", e)
