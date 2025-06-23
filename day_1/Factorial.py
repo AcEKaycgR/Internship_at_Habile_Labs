ea07f7d4ee91130af26199a5f6f673eca0a8955d
Kintan_Ace <61554695+AcEKaycgR@users.noreply.github.com>
2025-06-23 12:33:20 +0530

    Update Factorial.py

--- day_1/Factorial.py
+++ day_1/Factorial.py
@@ -1,9 +1,12 @@
-def Factorial(n):
-    if n == 1:
-        return 1
-    else:
-        return n * Factorial(n - 1)
+try:
+    def Factorial(n):
+        if n == 1:
+            return 1
+        else:
+            return n * Factorial(n - 1)
 
 
-Num = int(input("Enter a number: "))
-print(Factorial(Num))
+    Num = int(input("Enter a number: "))
+    print(Factorial(Num))
+except ValueError:
+    print("Enter a non-negative integer.")
