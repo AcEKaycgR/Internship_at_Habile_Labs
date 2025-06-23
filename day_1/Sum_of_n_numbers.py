bd4a66ee0a0902f28b798fa5b682cdca0bfb41e4
Kintan_Ace <61554695+AcEKaycgR@users.noreply.github.com>
2025-06-23 12:37:21 +0530

    Update Sum_of_n_numbers.py

--- day_1/Sum_of_n_numbers.py
+++ day_1/Sum_of_n_numbers.py
@@ -1,2 +1,5 @@
-n = int(input("Enter a number: "))
-print("Sum:", sum(range(1, n + 1)))
+try:
+    n = int(input("Enter a number: "))
+    print("Sum:", sum(range(1, n + 1)))
+except ValueError:
+    print("Please enter a valid number.")
