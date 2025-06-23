7c29c8f2d29b35768dacfb803d473ea477ce95f1
Kintan_Ace <61554695+AcEKaycgR@users.noreply.github.com>
2025-06-23 12:33:50 +0530

    Update Find_max_in_list.py

--- day_1/Find_max_in_list.py
+++ day_1/Find_max_in_list.py
@@ -1,2 +1,5 @@
-nums = [int(input(f"Enter number {i + 1}: ")) for i in range(5)]
-print(f"Maximum : {max(nums)}")
+try:
+    nums = [int(input(f"Enter number {i + 1}: ")) for i in range(5)]
+    print("Maximum:", max(nums))
+except ValueError:
+    print("Enter only valid integers.")
