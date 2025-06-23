854b60531485a4e1ae01d1f51d8525d22cbbb02f
Kintan_Ace <61554695+AcEKaycgR@users.noreply.github.com>
2025-06-23 12:36:09 +0530

    Update Print_all_prime.py

--- day_1/Print_all_prime.py
+++ day_1/Print_all_prime.py
@@ -1,7 +1,11 @@
-Num = int(input("Enter a number: "))
-for x in range(2, Num + 1):
-    for i in range(2, int(x ** 0.5) + 1):
-        if x % i == 0:
-            break
-    else:
-        print(x, end=" ")
+try:
+    n = int(input("Enter n: "))
+    for num in range(2, n + 1):
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                break
+        else:
+            print(num, end=" ")
+    print()
+except ValueError:
+    print("Please enter a valid number.")
