f76818fe0109246ff7cdb8269630184bba13ca40
Kintan_Ace <61554695+AcEKaycgR@users.noreply.github.com>
2025-06-23 12:33:33 +0530

    Update Fibonacci.py

--- day_1/Fibonacci.py
+++ day_1/Fibonacci.py
@@ -1,6 +1,12 @@
-Num = int(input("Enter a number: "))
-a = 0
-b = 1
-for _ in range(Num):
-    print(a, end=" ")
-    a, b = b, a + b
+try:
+    Num = int(input("Enter a number: "))
+    a = 0
+    b = 1
+    for _ in range(Num):
+        print(a, end=" ")
+        a, b = b, a + b
+except ValueError:
+    print("Not the right format please enter in the format '1' ")
+else:
+    print("Unknown error please try again")
+
