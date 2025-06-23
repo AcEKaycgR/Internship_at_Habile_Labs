f4f6738787151689332104162557696ae90b7278
Kintan_Ace <61554695+AcEKaycgR@users.noreply.github.com>
2025-06-23 12:34:01 +0530

    Update Find_second_largest.py

--- day_1/Find_second_largest.py
+++ day_1/Find_second_largest.py
@@ -1,10 +1,13 @@
-n = int(input("Enter how many numbers: "))
-Num = [int(input(f"Enter number {i + 1}: ")) for i in range(n)]
-max_num = Num[0]
-second_max = Num[1]
+try:
+    n = int(input("Enter how many numbers: "))
+    Num = [int(input(f"Enter number {i + 1}: ")) for i in range(n)]
+    max_num = Num[0]
+    second_max = Num[1]
 
-for i in Num:
-    if i > max_num:
-        second_max = max_num
-        max_num = i
-print(second_max)
+    for i in Num:
+        if i > max_num:
+            second_max = max_num
+            max_num = i
+    print(second_max)
+except ValueError:
+    print("Enter valid integers.")
